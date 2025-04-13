eac5270800259f610100c59fb6e1538656350ef3
zeinashaarawy <122227646+zeinashaarawy@users.noreply.github.com>
2025-04-13 22:36:22 +0200

    Update authentication.js

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,28 +1,25 @@
-const jwt = require("jsonwebtoken");
-const secretKey = process.env.SECRET_KEY
+const jwt = require('jsonwebtoken');
 
-module.exports = function authenticationMiddleware(req, res, next) {
-  const cookie = req.cookies;// if not working then last option req.headers.cookie then extract token
-  console.log('inside auth middleware')
-  // console.log(cookie);
+// Protect routes with authentication
+const protect = (req, res, next) => {
+  let token;
 
-  if (!cookie) {
-    return res.status(401).json({ message: "No Cookie provided" });
-  }
-  const token = cookie.token;
-  if (!token) {
-    return res.status(405).json({ message: "No token provided" });
-  }
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      token = req.headers.authorization.split(' ')[1]; // Extract token
 
-  jwt.verify(token, secretKey, (error, decoded) => {
-    if (error) {
-      return res.status(403).json({ message: "Invalid token" });
+      const decoded = jwt.verify(token, process.env.JWT_SECRET); // Verify the token
+      req.user = decoded; // Attach user info to the request
+
+      next();
+    } catch (error) {
+      res.status(401).json({ message: 'Not authorized, token failed' });
     }
+  }
 
-    // Attach the decoded user ID to the request object for further use
-    //console.log(decoded.user)
-    
-    req.user = decoded.user;
-    next();
-  });
+  if (!token) {
+    res.status(401).json({ message: 'Not authorized, no token' });
+  }
 };
+
+module.exports = { protect };
