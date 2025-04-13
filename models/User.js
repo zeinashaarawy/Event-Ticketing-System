25804f0ce8713585b5f2a5554426cfc7416046a7
JanaSaeed0 <jannasaeed2005@gmail.com>
2025-04-13 22:19:34 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  profilePicture: { type: String },
   password: { type: String, required: true },
-  role: { 
-    type: String, 
-    enum: ['Standard User', 'Organizer', 'System Admin'], 
-    default: 'Standard User' 
-  },
-  createdAt: { type: Date, default: Date.now }
+  role: { type: String, enum: ['user', 'organizer', 'admin'], default: 'user' },
+});
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
 module.exports = mongoose.model('User', userSchema);
