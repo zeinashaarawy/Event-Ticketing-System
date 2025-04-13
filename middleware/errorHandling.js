3d2d953a5f64feb79ea54ab9f6189064507419bd
zeinashaarawy <122227646+zeinashaarawy@users.noreply.github.com>
2025-04-13 22:40:39 +0200

    Update errorHandling.js

--- middleware/errorHandling.js
+++ middleware/errorHandling.js
@@ -1,9 +1,10 @@
+// Middleware to handle errors globally
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode; // Default to 500 if no status code
 
   res.status(statusCode).json({
     message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack, // Hide stack trace in production
   });
 };
 
