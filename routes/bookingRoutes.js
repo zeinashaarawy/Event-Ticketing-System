447231c998e7983f1aed623df73e3f453e480e23
zeinashaarawy <122227646+zeinashaarawy@users.noreply.github.com>
2025-04-13 22:50:17 +0200

    Update bookingRoutes.js

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { protect, restrictTo } = require('../middleware/authMiddleware');
+const { protect, restrictTo } = require('../middleware/authentication');
 const {
   bookTickets,
   getUserBookings,
