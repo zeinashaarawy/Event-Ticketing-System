fddcbcd1b1f729a8ba373c8271c292eea3f52e91
Judy747 <judy.ezzat2007@gmail.com>
2025-04-13 22:33:15 +0200

    Update authorization.js

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,1 +1,10 @@
-
+module.exports= function authorizationMiddleware(roles) {
+    return (req, res, next) => {
+      console.log('req:',req.user)
+      const userRole = req.user.role;
+      if (!roles.includes(userRole))
+        return res.status(403).json("unauthorized access");
+      // console.log('authormid')
+      next();
+    };
+  }
