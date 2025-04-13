2991b0e10416a00dee3b25d4da9d5546e5c0f879
JanaSaeed0 <jannasaeed2005@gmail.com>
2025-04-13 22:50:00 +0200

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authController');
+const { registerUser, loginUser } = require('../controllers/userController');
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 module.exports = router;
