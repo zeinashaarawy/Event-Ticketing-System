1b77836bfd49f0909c00fcec3bc23b13c724ebbb
Judy747 <judy.ezzat2007@gmail.com>
2025-04-18 15:48:12 +0200

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+
+
 const {
   getProfile,
   updateProfile,
@@ -10,24 +12,34 @@ const {
   deleteUser,
   login,
   registerUser,
-  forgetPassword
+  forgetPassword,
+  getOrganizerEvents,
+  getEventAnalytics
 } = require('../controllers/userController');
 
 const { protect, authorizeRoles } = require('../middleware/authentication');
+const Event = require('../models/Event');
+
 
 // Public routes
 router.post('/register', registerUser);
-router.post('/login', login);
+router.post('/login', login); 
 router.put('/forgetPassword', forgetPassword);
 
 // Authenticated user routes
-router.get('/users/profile', protect, getProfile);
-router.put('/users/profile', protect, updateProfile);
+router.get('/profile', protect, getProfile);
+router.put('/profile', protect, updateProfile);
+
+
+router.get('/events', protect, authorizeRoles('organizer'), getOrganizerEvents);
+router.get('/events/analytics' , protect , authorizeRoles('organizer') , getEventAnalytics);
+
+
 
 // Admin-only routes
-router.get('/users', protect, authorizeRoles('admin'), getAllUsers);
-router.get('/users/:id', protect, authorizeRoles('admin'), getUserById);
-router.put('/users/:id', protect, authorizeRoles('admin'), updateUserRole);
-router.delete('/users/:id', protect, authorizeRoles('admin'), deleteUser);
+router.get('/', protect, authorizeRoles('admin'), getAllUsers);
+router.get('/:id', protect, authorizeRoles('admin'), getUserById);
+router.put('/:id', protect, authorizeRoles('admin'), updateUserRole);
+router.delete('/:id', protect, authorizeRoles('admin'), deleteUser);
 
 module.exports = router;
