6c73700a6917b2f07534a16ec3366f31134b34f0
Judy747 <judy.ezzat2007@gmail.com>
2025-04-19 14:34:19 +0200

    Update eventRoutes.js

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const { createEvent, getEventById, getAllEvents, updateEvent, deleteEvent } = require('../controllers/eventController');
+
+const { 
+    createEvent, 
+    getEventById, 
+    getApprovedEvents, 
+    getAllEvents,
+    updateEvent, 
+    deleteEvent 
+} = require('../controllers/eventController');
 const { protect, authorizeRoles } = require('../middleware/authentication');
 
-// Organizer creates a new event
-router.post('/events', protect, authorizeRoles('organizer'), createEvent);
 
-// Public can view event by ID
-router.get('/events', getAllEvents);
+router.post('/', protect, authorizeRoles('organizer', 'admin'), createEvent);
 
-router.get('/events/:id', getEventById);
-router.put('/events/:id', protect, authorizeRoles('organizer', 'admin'), updateEvent);
-router.delete('/events/:id', protect, authorizeRoles('organizer', 'admin'), deleteEvent);
+router.get('/all', protect, authorizeRoles('admin'), getAllEvents); 
+
+//didn't work it requires a token but it shouldn't require and it's not read 
+router.get('/', getApprovedEvents); 
+  
+
+router.get('/:id', getEventById);
+
+router.put('/:id', protect, authorizeRoles('organizer', 'admin'), updateEvent);
+
+
+router.delete('/:id', protect, authorizeRoles('organizer', 'admin'), deleteEvent);
 
 module.exports = router;
