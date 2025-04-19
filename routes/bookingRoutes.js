6740c1bbd56e98f706dae154cc15645eb8be5d69
Judy747 <judy.ezzat2007@gmail.com>
2025-04-19 14:36:05 +0200

    Update bookingRoutes.js

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -3,16 +3,12 @@ const router = express.Router();
 const { protect, authorizeRoles } = require('../middleware/authentication');
 const {
   bookTickets,
-  getUserBookings,
   getBookingDetails,
   cancelBooking,
 } = require('../controllers/bookingController');
 
 // Book tickets
 router.post('/', protect, authorizeRoles('user'), bookTickets);
-
-// Get all bookings for a user
-router.get('/users/bookings', protect, authorizeRoles('user'), getUserBookings);
 
 
 // Get booking details by ID
