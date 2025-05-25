856871917196993f14d9ac80d26697735c19df7e
Judy747 <judy.ezzat2007@gmail.com>
2025-05-25 14:00:43 +0300

    Update UpdateProfileForm.jsx

--- Client/react-project/src/components/profile/UpdateProfileForm.jsx
+++ Client/react-project/src/components/profile/UpdateProfileForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useAuth } from '../../context/authContext';
+import { useAuth } from '../../context/AuthContext';
 import { toast } from 'react-toastify';
 
 const UpdateProfileForm = ({ user, onCancel, onSuccess }) => {
